4930362f2ace75e634f2f8ddcbd71a42daad6a0b
Christian Biasuzzi <christian.biasuzzi@techrain.eu>
2021-03-04 15:33:20 +0100

    removes from DiffSVGWriter code copyed from DefaultSVGWriter
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@techrain.eu>

--- src/main/java/com/powsybl/diff/server/DiffSVGWriter.java
+++ src/main/java/com/powsybl/diff/server/DiffSVGWriter.java
@@ -2,30 +2,31 @@ package com.powsybl.diff.server;
 
 import com.powsybl.sld.layout.LayoutParameters;
 import com.powsybl.sld.library.ComponentLibrary;
-import com.powsybl.sld.library.ComponentMetadata;
-import com.powsybl.sld.model.BusCell;
 import com.powsybl.sld.model.FeederNode;
 import com.powsybl.sld.svg.DefaultSVGWriter;
 import com.powsybl.sld.svg.DiagramLabelProvider;
 import com.powsybl.sld.svg.GraphMetadata;
-import com.powsybl.sld.svg.InitialValue;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
-import static com.powsybl.sld.library.ComponentTypeName.ARROW;
-import static com.powsybl.sld.svg.DiagramStyles.*;
+import static com.powsybl.sld.svg.DiagramStyles.ARROW_ACTIVE_CLASS;
+import static com.powsybl.sld.svg.DiagramStyles.ARROW_REACTIVE_CLASS;
 
 /**
  * @author Christian Biasuzzi <christian.biasuzzi@techrain.eu>
  */
 public class DiffSVGWriter extends DefaultSVGWriter {
 
-    private List<String> vlDiffs;
-    private List<String> branchDiffs;
+    public static final String DIFFERENT_SUFFIX = "-diff1";
+    public static final String SAME_SUFFIX = "-diff2";
+
+    private final List<String> vlDiffs;
+    private final List<String> branchDiffs;
 
     public DiffSVGWriter(ComponentLibrary componentLibrary, LayoutParameters layoutParameters, List<String> vlDiffs, List<String> branchDiffs) {
         super(componentLibrary, layoutParameters);
@@ -35,73 +36,18 @@ public class DiffSVGWriter extends DefaultSVGWriter {
 
     @Override
     protected void insertArrowsAndLabels(String prefixId, String wireId, List<Double> points, Element root, FeederNode feederNode, GraphMetadata metadata, DiagramLabelProvider initProvider, boolean feederArrowSymmetry) {
-        InitialValue init = initProvider.getInitialValue(feederNode);
+        super.insertArrowsAndLabels(prefixId, wireId, points, root, feederNode, metadata, initProvider, feederArrowSymmetry);
 
-        boolean arrowSymmetry = feederNode.getDirection() == BusCell.Direction.TOP || feederArrowSymmetry;
-
-        Optional<String> label1 = arrowSymmetry ? init.getLabel1() : init.getLabel2();
-        Optional<DiagramLabelProvider.Direction> direction1 = arrowSymmetry ? init.getArrowDirection1() : init.getArrowDirection2();
-
-        Optional<String> label2 = arrowSymmetry ? init.getLabel2() : init.getLabel1();
-        Optional<DiagramLabelProvider.Direction> direction2 = arrowSymmetry ? init.getArrowDirection2() : init.getArrowDirection1();
-
-        int iArrow1 = arrowSymmetry ? 1 : 2;
-        int iArrow2 = arrowSymmetry ? 2 : 1;
-
-        //diff specific: if the diff list contains this feeder id
-        final boolean diffStyleOverrides = branchDiffs.contains(feederNode.getId());
-
-        // we draw the arrow only if value 1 is present
-        label1.ifPresent(lb ->
-                drawArrowAndLabel(prefixId, wireId, points, root, lb, init.getLabel3(), direction1, 0, iArrow1, metadata, diffStyleOverrides));
-
-        // we draw the arrow only if value 2 is present
-        label2.ifPresent(lb -> {
-            double shiftArrow2 = 2 * metadata.getComponentMetadata(ARROW).getSize().getHeight();
-            drawArrowAndLabel(prefixId, wireId, points, root, lb, init.getLabel4(),
-                    direction2, shiftArrow2, iArrow2, metadata,
-                    diffStyleOverrides);
-        });
+        //replace arrows css classes with the diff ones
+        String diffSuffix = branchDiffs.contains(feederNode.getId()) ? SAME_SUFFIX : DIFFERENT_SUFFIX;
+        for (int i = 0; i < root.getElementsByTagName(GROUP).getLength(); i++) {
+            Node gNode = root.getElementsByTagName(GROUP).item(i);
+            if (gNode instanceof Element) {
+                List<String> classesList = Arrays.asList(((Element) gNode).getAttribute(CLASS).split(" "));
+                Collections.replaceAll(classesList, ARROW_ACTIVE_CLASS, ARROW_ACTIVE_CLASS + diffSuffix);
+                Collections.replaceAll(classesList, ARROW_REACTIVE_CLASS, ARROW_REACTIVE_CLASS + diffSuffix);
+                ((Element) gNode).setAttribute(CLASS, String.join(" ", classesList));
+            }
+        }
     }
-
-    private void drawArrowAndLabel(String prefixId, String wireId, List<Double> points, Element root,
-                                   String labelR, Optional<String> labelL, Optional<DiagramLabelProvider.Direction> dir, double shift, int iArrow,
-                                   GraphMetadata metadata, boolean isdiffStyleOverrides) {
-        ComponentMetadata cd = metadata.getComponentMetadata(ARROW);
-
-        double shX = cd.getSize().getWidth() + LABEL_OFFSET;
-        double shY = cd.getSize().getHeight() / 2;
-
-        double y1 = points.get(1);
-        double y2 = points.get(3);
-
-        Element g = root.getOwnerDocument().createElement(GROUP);
-        String arrowWireId = wireId + "_ARROW" + iArrow;
-        g.setAttribute("id", arrowWireId);
-        transformArrow(points, cd.getSize(), shift, g);
-
-        insertArrowSVGIntoDocumentSVG(prefixId, g, y1 > y2 ? 180 : 0);
-        Element label = createLabelElement(labelR, shX, shY, 0, g);
-        g.appendChild(label);
-
-        List<String> styles = new ArrayList<>(3);
-        componentLibrary.getComponentStyleClass(ARROW).ifPresent(styles::add);
-
-        //diff specific: set the style suffix
-        String diffSuffix = isdiffStyleOverrides ? "-diff2" : "-diff1";
-        styles.add(iArrow == 1 ? ARROW_ACTIVE_CLASS + diffSuffix : ARROW_REACTIVE_CLASS + diffSuffix);
-        dir.ifPresent(direction -> styles.add(direction == DiagramLabelProvider.Direction.UP ? UP_CLASS : DOWN_CLASS));
-        g.setAttribute(CLASS, String.join(" ", styles));
-
-        labelL.ifPresent(s -> {
-            Element labelLeft = createLabelElement(s, -LABEL_OFFSET, shY, 0, g);
-            labelLeft.setAttribute(STYLE, "text-anchor:end");
-            g.appendChild(labelLeft);
-        });
-
-        root.appendChild(g);
-        metadata.addArrowMetadata(new GraphMetadata.ArrowMetadata(arrowWireId, wireId, layoutParameters.getArrowDistance()));
-
-    }
-
 }
